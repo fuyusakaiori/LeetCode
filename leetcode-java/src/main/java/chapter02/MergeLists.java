57973ae9d5abc5f3b83ba825c96b91942955905a
zhouhang.shen <zhouhang@jiduauto.com>
2024-12-16 20:40:18 +0800

    refactor:(data structure list)

--- leetcode-java/src/main/java/chapter02/MergeLists.java
+++ leetcode-java/src/main/java/chapter02/MergeLists.java
@@ -18,49 +18,47 @@ public class MergeLists {
     private static ListNode mergeTwoSortedLists(ListNode list1, ListNode list2){
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
-        while (list1 != null && list2 != null){
-            if (list1.value < list2.value){
+        while (list1 != null && list2 != null) {
+            if (list1.value < list2.value) {
                 current.next = list1;
                 list1 = list1.next;
-            }else{
+            } else{
                 current.next = list2;
                 list2 = list2.next;
             }
             current = current.next;
         }
-        if (list1 != null) current.next = list1;
-        if (list2 != null) current.next = list2;
+        if (list1 != null) {
+            current.next = list1;
+        }
+        if (list2 != null) {
+            current.next = list2;
+        }
         return dummy.next;
     }
-
 
     /**
      * <h3>思路: 找到合并链表的起始位置和终止位置就行</h3>
      */
     private static ListNode mergeBetweenLists(ListNode list1, ListNode list2, int left, int right){
-        ListNode head = list1;
-        ListNode previous = null;
-        ListNode next = null;
+        ListNode dummy = new ListNode(0, list1);
+        ListNode current = dummy, tail = list2;
+        ListNode leftNode = null, rightNode = null;
         int index = 0;
-        // 先找到合并链表的起始位置
-        while(index < left - 1){
-            list1 = list1.next;
-            index++;
+        for (index = 0; index < left; index++) {
+            current = current.next;
         }
-        // 记录起始结点
-        previous = list1;
-        // 找到合并链表为结束位置
-        while(index < right + 1){
-            list1 = list1.next;
-            index++;
+        leftNode = current;
+        for (index = left; index < right + 1; index++) {
+            current = current.next;
         }
-        // 找到第二个链表的尾部
-        while(list2.next != null) list2 = list2.next;
-        next = list1;
-        // 开始合并链表
-        previous.next = list2;
-        list2.next = next;
-        return head;
+        rightNode = current.next;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        leftNode.next = list2;
+        tail.next = rightNode;
+        return dummy.next;
     }
 
     /**
@@ -74,12 +72,13 @@ public class MergeLists {
     }
 
     private static ListNode fork(ListNode[] lists, int left, int right){
-        if (left >= right)
+        if (left >= right) {
             return lists[left];
+        }
         int mid = left + ((right - left) >> 1);
-        ListNode leftList = fork(lists, left, mid);
-        ListNode rightList = fork(lists, mid + 1, right);
-        return mergeTwoSortedLists(leftList, rightList);
+        ListNode first = fork(lists, left, mid);
+        ListNode second = fork(lists, mid + 1, right);
+        return mergeTwoSortedLists(first, second);
     }
 
 
