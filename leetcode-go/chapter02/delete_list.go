62cb0220394fb865190cfc5de155f6e71072e393
fuyusakaiori <fuyusakaiori>
2024-12-25 20:41:20 +0800

    feat:(data structure list)

--- leetcode-go/chapter02/delete_list.go
+++ leetcode-go/chapter02/delete_list.go
@@ -1,11 +1,11 @@
 package main
 
-import "leetcode-go/node"
+import nodes "leetcode-go/node"
 
 type ListDeleter struct {}
 
 // 删除排序链表中的重复元素
-func (deleter *ListDeleter) DeleteDuplicates(head *node.ListNode) *node.ListNode {
+func (deleter *ListDeleter) DeleteDuplicates(head *nodes.ListNode) *nodes.ListNode {
 	current := head
 	for current != nil && current.Next != nil {
 		if current.Val == current.Next.Val {
@@ -18,8 +18,8 @@ func (deleter *ListDeleter) DeleteDuplicates(head *node.ListNode) *node.ListNode
 }
 
 // 删除排序链表中的重复元素 II
-func (deleter *ListDeleter) DeleteDuplicatesII(head *node.ListNode) *node.ListNode {
-	dummy := &node.ListNode{Val: 0, Next: head}
+func (deleter *ListDeleter) DeleteDuplicatesII(head *nodes.ListNode) *nodes.ListNode {
+	dummy := &nodes.ListNode{Val: 0, Next: head}
 	current := dummy
 	for current.Next != nil && current.Next.Next != nil {
 		if current.Next.Val == current.Next.Next.Val {
@@ -35,9 +35,9 @@ func (deleter *ListDeleter) DeleteDuplicatesII(head *node.ListNode) *node.ListNo
 }
 
 // 删除链表的倒数第 N 个节点: 计算长度
-func (deleter *ListDeleter) RemoveNthFromEnd(head *node.ListNode, n int) *node.ListNode {
+func (deleter *ListDeleter) RemoveNthFromEnd(head *nodes.ListNode, n int) *nodes.ListNode {
 	length := deleter.getLength(head)
-	dummy := &node.ListNode{Val: 0, Next: head}
+	dummy := &nodes.ListNode{Val: 0, Next: head}
 	current := dummy
 	for index := 0; index < length - n; index++ {
 		current = current.Next
@@ -47,8 +47,8 @@ func (deleter *ListDeleter) RemoveNthFromEnd(head *node.ListNode, n int) *node.L
 }
 
 // 删除链表的倒数第 N 个节点: 双指针
-func (deleter *ListDeleter) RemoveNthFromEndDoublePointer(head *node.ListNode, n int) *node.ListNode {
-	dummy := &node.ListNode{Val: 0, Next: head}
+func (deleter *ListDeleter) RemoveNthFromEndDoublePointer(head *nodes.ListNode, n int) *nodes.ListNode {
+	dummy := &nodes.ListNode{Val: 0, Next: head}
 	first, second := head, dummy
 	for index := 0; index < n; index++ {
 		first = first.Next
@@ -61,11 +61,31 @@ func (deleter *ListDeleter) RemoveNthFromEndDoublePointer(head *node.ListNode, n
 	return dummy.Next
 }
 
-func (deleter *ListDeleter) getLength(head *node.ListNode) int {
+func (deleter *ListDeleter) getLength(head *nodes.ListNode) int {
 	length := 0
 	for head != nil {
 		length++
 		head = head.Next
 	}
 	return length
+}
+
+// 移除链表元素
+func (deleter *ListDeleter) RemoveElements(head *nodes.ListNode, target int) *nodes.ListNode {
+	dummy := &nodes.ListNode{Val: 0, Next: head}
+	current := dummy
+	for current.Next != nil {
+		if current.Next.Val == target {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+	return dummy.Next
+}
+
+// 删除链表中的节点
+func (deleter *ListDeleter) DeleteNode(node *nodes.ListNode)  {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
 }
